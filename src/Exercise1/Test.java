0a08200682703b342077710ba1a35f3ba655d7bd
Hanren Tu <tu.hanren@gmail.com>
2021-10-06 21:29:54 -0400

    Question 1 done

--- src/Exercise1/Test.java
+++ src/Exercise1/Test.java
@@ -1,90 +1,86 @@
 package Exercise1;
 
 import javax.swing.*;
-import java.security.SecureRandom;
-import java.util.Arrays;
-
-import static java.lang.Double.parseDouble;
+import java.util.Random;
 
 public class Test {
-
-    private String[] questions;
-    private String[] choices;
-    private String[] userSelects = {"","","","",""};
-    private String userSelect = null;
-    private String[] correctAnswer = {"A","A","A","A","A"};
-    private SecureRandom random;
-    private int correctCount = 0;
-    private int wrongCount = 0;
-    //private double correctRate = 0;
-    //constructor
-    public Test(String[] questions, String[] choices, String[] userSelects) {
-        this.questions = questions;
-        this.choices = choices;
-        this.userSelects = userSelects;
-        this.random = new SecureRandom();
-        this.correctCount = correctCount;
-        this.wrongCount = wrongCount;
-        //this.correctRate = correctRate;
-    }
-    //getters
-    public int getCorrectAnswer() { return this.correctCount; }
-    public int getWrongAnswer() { return this.wrongCount; }
-    //public double getCorrectRate() { return this.correctRate; }
+    //declare
+    String[] questions = {"What is 1?\n A.123\n B.123\n C.123\n D.123",
+            "What is 2?\n A.123\n B.123\n C.123\n D.123",
+            "What is 3?\n A.123\n B.123\n C.123\n D.123",
+            "What is 4?\n A.123\n B.123\n C.123\n D.123",
+            "What is 5?\n A.123\n B.123\n C.123\n D.123"
+    };
+    String[] choices = {"A", "B", "C", "D"};
+    String[] correctAnswer = {"A","A","A","A","A"};
+    String[] userSelects = {"","","","",""};
+    String userSelect;
+    boolean check;
+    int correctCount = 0;
+    int wrongCount = 0;
+    float correctRate;
     //methods
     public void simulateQuestion() {
 
         for (int i = 0; i <questions.length; i++) {
-             userSelect = (String) JOptionPane.showInputDialog(null,
-                     questions[i], "Test",
-                     JOptionPane.INFORMATION_MESSAGE,null,
-                     choices, choices[0]);
-
+            userSelect = (String) JOptionPane.showInputDialog(null,
+                    questions[i], "Test",
+                    JOptionPane.INFORMATION_MESSAGE, null,
+                    choices, choices[0]);
              userSelects[i] = userSelect;
-
+             checkAnswer(i);
              JOptionPane.showMessageDialog(null,
                      generateMessage(i),
                      "Message",
                      JOptionPane.PLAIN_MESSAGE);
         }
-        //correctRate = correctCount / questions.length * 100;
-    }
-    public boolean checkAnswer(int i) {
-        if (inputAnswer(i).equals(correctAnswer[i])){
-            correctCount += 1;
 
-            return true;
+    }
+    public void checkAnswer(int i) {
+        if (userSelects[i].equals(correctAnswer[i])){
+            check = true;
+            correctCount += 1;
         }
         else {
+            check = false;
             wrongCount += 1;
-
-            return false;
         }
     }
     public String generateMessage(int i) {
-        if (checkAnswer(i)) {
-            switch(1 + random.nextInt(4)) {
+        Random random = new Random();
+        if (check) {
+            switch(1+random.nextInt(4)) {
                 case 1 -> { return "Excellent!"; }
                 case 2 -> { return "Good!"; }
                 case 3 -> { return "Keep up the good work!"; }
                 case 4 -> { return "Nice work!"; }
-                default -> {return  "err"; }
+                default -> { return  "Good."; }
             }
+
         }
         else {
-            switch(1 + random.nextInt(4)) {
+            switch(1+random.nextInt(4)) {
                 case 1 -> { return "No. Please try again!"; }
                 case 2 -> { return "Wrong. Try once more!"; }
                 case 3 -> { return "Don't give up!"; }
                 case 4 -> { return "No. Keep Trying!"; }
-                default -> {return  "err"; }
+                default -> { return  "No. Keep Trying."; }
             }
         }
     }
-    public String inputAnswer(int i) {
+    public void inputAnswer() {
+        simulateQuestion();
+        correctRate = (float)correctCount/ questions.length;//MUST process or it will be 0!
+        String message = "Your correct count is: " + correctCount + "\n" +
+                         "Your wrong count is: " + wrongCount + "\n" +
+                         "Your correct rate is: " + correctRate;//Double.parseDouble(String.valueOf(correctCount))/5*100
+        JOptionPane.showMessageDialog(null, message);
 
-        return  userSelects[i];
-
+//        System.out.println(correctCount);
+//        System.out.println(wrongCount);
+//        System.out.printf("""
+//                Your correct count is: %d
+//                Your wrong count is: %d
+//                Your correct rate is: %.2f \s""", correctCount, wrongCount,correctRate);
     }
-
 }
--- src/Exercise1/TestApp.java
+++ src/Exercise1/TestApp.java
@@ -1,32 +1,13 @@
 package Exercise1;
-import javax.swing.*;
-import java.util.Arrays;
+
 
 public class TestApp {
 
     public static void main(String[] args) {
 
-        String question[] = {"What is 1?\n A.123\n B.123\n C.123\n D.123",
-                "What is 2?\n A.123\n B.123\n C.123\n D.123",
-                "What is 3?\n A.123\n B.123\n C.123\n D.123",
-                "What is 4?\n A.123\n B.123\n C.123\n D.123",
-                "What is 5?\n A.123\n B.123\n C.123\n D.123"};
+        Test obj = new Test();
 
-        String choices[] = {"A", "B", "C", "D"};
+        obj.inputAnswer();
 
-        String userSelects[] = {"","","","",""};
-
-
-        Test obj = new Test(question, choices, userSelects);
-        obj.simulateQuestion();
-        System.out.printf("""
-                        number of correct: %d
-                        number of wrong: %d
-                        correct rate:%.2f%%""",
-                obj.getCorrectAnswer(),
-                obj.getWrongAnswer(),
-                Double.parseDouble(String.valueOf(obj.getCorrectAnswer()))/5*100
-                //obj.getCorrectRate()
-        );
     }
 }
