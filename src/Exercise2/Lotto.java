6cf35a20605985e38d6fcc0af4053bd0db0a0b7d
Hanren Tu <tu.hanren@gmail.com>
2021-10-09 00:59:20 -0400

    correction

--- src/Exercise2/Lotto.java
+++ src/Exercise2/Lotto.java
@@ -2,44 +2,23 @@ package Exercise2;
 
 import java.util.Random;
 import java.util.stream.IntStream;
-import javax.swing.JOptionPane;
+
 
 public class Lotto {
     //declare
     int[] ranArray = new int[3];
-    int arraySum;
-    int userChoice;
+    int arraySum = 0;
     //constructor
     public Lotto() {
-    }
-    //method for the random array and the sum
-    public int generateArray() {
         Random random = new Random();
         for (int i = 0; i < ranArray.length; i++) {
             ranArray[i] = random.nextInt(9-1)+1;
         }
         arraySum = IntStream.of(ranArray).sum();
+    }
+    //method for the random array and the sum
+    public int generateArraySum() {
         return arraySum;
     }
-    public void simulateLotto() {
-        for (int i = 0; i <= 5; i++) {
-            //collecting user input and convert to Int type
-            userChoice = Integer.parseInt(JOptionPane.showInputDialog(null, "Try a number between 3 and 27"));
-            //response for different conditions
-            if(userChoice == generateArray()) {
-                String message = "You Win!\n" +
-                                "The Lucky Number is: " + generateArray();
-                JOptionPane.showMessageDialog(null,message,"Lotte",JOptionPane.INFORMATION_MESSAGE);
-                break;
-            }
-            else if(userChoice != generateArray()) {
-                String message = "Whoops! The lucky Number is: " + generateArray();
-                JOptionPane.showMessageDialog(null,message,"Lotte",JOptionPane.ERROR_MESSAGE);
-            }
-            if(i == 4) {
-                JOptionPane.showMessageDialog(null, "Computer Wins!","Lotte",JOptionPane.WARNING_MESSAGE);
-                break;
-            }
-        }
-    }
 }
+
--- src/Exercise2/LottoApp.java
+++ src/Exercise2/LottoApp.java
@@ -1,10 +1,37 @@
 package Exercise2;
+import javax.swing.JOptionPane;
 
 public class LottoApp {
 
     public static void main(String[] args) {
-
-        Lotto ranArray = new Lotto();
-        ranArray.simulateLotto();
+        Lotto lotto = null;
+        int userChoice = 0;
+        boolean wins = false;
+        for (int i = 0; i < 5; i++) {
+            lotto = new Lotto();
+            int arraySum = lotto.generateArraySum();
+            //collecting user input and convert to Int type
+            userChoice = Integer.parseInt(JOptionPane.showInputDialog(null, "Try a number between 3 and 27"));
+            while (userChoice < 3 || userChoice > 27){
+                JOptionPane.showMessageDialog(null,"Please enter a number between 3 - 27");
+                userChoice = Integer.parseInt(JOptionPane.showInputDialog(null, "Try a number between 3 and 27"));
+            }
+            //response for different conditions
+            if(userChoice == arraySum) {
+                wins = true;
+                String message = "You Win!\n" +
+                        "The Lucky Number is: " + arraySum;
+                JOptionPane.showMessageDialog(null,message,"Lotte",JOptionPane.INFORMATION_MESSAGE);
+                break;
+            }
+            else {
+                String message = "Whoops! The lucky Number is: " + arraySum;
+                JOptionPane.showMessageDialog(null,message,"Lotte",JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        if (!wins){
+            JOptionPane.showMessageDialog(null, "Computer Wins!","Lotte",JOptionPane.WARNING_MESSAGE);
+        }
     }
 }
+
