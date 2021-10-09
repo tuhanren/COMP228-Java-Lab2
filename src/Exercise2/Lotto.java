c6de05b8010196299ee8332667c278d93d9cdbb2
Hanren Tu <tu.hanren@gmail.com>
2021-10-09 01:06:32 -0400

    final v2

--- src/Exercise2/Lotto.java
+++ src/Exercise2/Lotto.java
@@ -1,24 +1,23 @@
 package Exercise2;
 
 import java.util.Random;
-import java.util.stream.IntStream;
+
 
 
 public class Lotto {
     //declare
     int[] ranArray = new int[3];
-    int arraySum = 0;
     //constructor
     public Lotto() {
         Random random = new Random();
         for (int i = 0; i < ranArray.length; i++) {
             ranArray[i] = random.nextInt(9-1)+1;
         }
-        arraySum = IntStream.of(ranArray).sum();
+        //arraySum = IntStream.of(ranArray).sum();
     }
     //method for the random array and the sum
-    public int generateArraySum() {
-        return arraySum;
+    public int[] generateArray() {
+        return ranArray;
     }
 }
 
--- src/Exercise2/LottoApp.java
+++ src/Exercise2/LottoApp.java
@@ -1,15 +1,21 @@
 package Exercise2;
 import javax.swing.JOptionPane;
-
+import java.util.stream.IntStream;
 public class LottoApp {
 
     public static void main(String[] args) {
-        Lotto lotto = null;
+        //declare
         int userChoice = 0;
-        boolean wins = false;
+        boolean win = false;
+        Lotto lotto = null;
+
         for (int i = 0; i < 5; i++) {
+            //create a new obj
             lotto = new Lotto();
-            int arraySum = lotto.generateArraySum();
+            //get the array from Lotto class
+            int[] array = lotto.generateArray();
+            //sum the array with IntStream lib
+            int arraySum = IntStream.of(array).sum();
             //collecting user input and convert to Int type
             userChoice = Integer.parseInt(JOptionPane.showInputDialog(null, "Try a number between 3 and 27"));
             while (userChoice < 3 || userChoice > 27){
@@ -18,7 +24,7 @@ public class LottoApp {
             }
             //response for different conditions
             if(userChoice == arraySum) {
-                wins = true;
+                win = true;
                 String message = "You Win!\n" +
                         "The Lucky Number is: " + arraySum;
                 JOptionPane.showMessageDialog(null,message,"Lotte",JOptionPane.INFORMATION_MESSAGE);
@@ -29,7 +35,7 @@ public class LottoApp {
                 JOptionPane.showMessageDialog(null,message,"Lotte",JOptionPane.ERROR_MESSAGE);
             }
         }
-        if (!wins){
+        if (!win){
             JOptionPane.showMessageDialog(null, "Computer Wins!","Lotte",JOptionPane.WARNING_MESSAGE);
         }
     }
